44f9e4a42b898dd1819d527900f3c671ce0d5f93
elif <elifdiril6@gmail.com>
2021-06-09 14:32:18 +0300

    bug fixed

--- src/components/campaignElement.js
+++ src/components/campaignElement.js
@@ -7,33 +7,33 @@ export default class CampaignElement extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {modal: false, item: this.props.item}
+        this.state = { modal: false, item: this.props.item }
         this.toggle = this.toggle.bind(this);
     }
 
-    toggle = () => this.setState({modal: !this.state.modal})
+    toggle = () => this.setState({ modal: !this.state.modal })
 
 
     render() {
         return (
             <div width="100px">
                 <Container className="themed-container" fluid="sm">
-                <br></br>
-                    <Col sm="6" md={{ offset: 2 }} style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '85%', width: '50%', paddingTop: '1%'}}>
+                    <br></br>
+                    <Col sm="6" md={{ offset: 2 }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '85%', width: '50%', paddingTop: '1%' }}>
                         <Card>
                             <Row >
-                                <Col xs="2">                                   
-                                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75%', paddingLeft: '40%'}}> {this.props.item.point} POINTS </div>
+                                <Col xs="2">
+                                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75%', paddingLeft: '40%' }}> {this.props.item.point} POINTS </div>
                                 </Col>
                                 <Col xs="10">
                                     <Row><h1>{this.props.item.name}</h1></Row>
                                     <Row><h3>{this.props.item.description}</h3></Row>
                                     <br></br>
-                                    <Row style={{ paddingRight: '1%'}}>
+                                    <Row style={{ paddingRight: '1%' }}>
                                         <Col><Button outline color="success" onClick={() => this.props.increasePoint(this.props.item.id)}>+</Button></Col>
-                                        <Col><Button outline color="danger" onClick= {() => this.props.decreasePoint(this.props.item.id)}>-</Button></Col>
-                                        <Col><Button outline color="warning" onClick = {this.toggle}>Update</Button></Col>
-                                        <Col><Button outline color="danger">Delete</Button></Col>
+                                        <Col><Button outline color="danger" onClick={() => this.props.decreasePoint(this.props.item.id)}>-</Button></Col>
+                                        <Col><Button outline color="warning" onClick={this.toggle}>Update</Button></Col>
+                                        <Col><Button outline color="danger" onClick={() => this.props.deleteCampaign(this.props.item.id)}>Delete</Button></Col>
                                     </Row>
                                     <br></br>
                                 </Col>
@@ -44,10 +44,10 @@ export default class CampaignElement extends Component {
                 </Container>
                 <br></br>
                 <ModalComponent
-                modal={this.state.modal}
-                toggle={this.toggle}
-                item={this.state.item}
-                updateCampaign={(newItem)=>this.props.updateCampaign(newItem, this.state.item.id)}/>
+                    modal={this.state.modal}
+                    toggle={this.toggle}
+                    item={this.state.item}
+                    updateCampaign={(newItem) => this.props.updateCampaign(newItem, this.state.item.id)} />
             </div>
         );
     }
--- src/pages/CampaignPage.js
+++ src/pages/CampaignPage.js
@@ -16,6 +16,13 @@ const CampaignList = [
         "createDate": "13/04/2021",
         "point": 4,
     },
+    {
+        "id": 2,
+        "name": "Campaign",
+        "description": "Campaign 3",
+        "createDate": "05/06/2021",
+        "point": 9,
+    },
 ]
 
 export default function CampaignPage() {
@@ -29,46 +36,59 @@ export default function CampaignPage() {
 
     const increasePoint = (index) => {
         if (index !== -1 && index !== null) {
-            let newCampList = JSON.parse(localStorage.getItem('item'));
-            let updatedElement = JSON.parse(localStorage.getItem('item'))[index].point + 1;
+            let newCampList = JSON.parse(item);
+            let updatedElement = JSON.parse(item)[index].point + 1;
 
             newCampList[index].point = updatedElement;
 
             localStorage.setItem('item', JSON.stringify(newCampList));
-            setItem(newCampList)
+            setItem(JSON.stringify(newCampList))
         }
     }
 
     const decreasePoint = (index) => {
         if (index !== -1 && index !== null) {
-            let newCampList = JSON.parse(localStorage.getItem('item'));
-            let updatedElement = JSON.parse(localStorage.getItem('item'))[index].point - 1;
+            let newCampList = JSON.parse(item);
+            let updatedElement = JSON.parse(item)[index].point - 1;
 
             newCampList[index].point = updatedElement;
 
             localStorage.setItem('item', JSON.stringify(newCampList));
-            setItem(newCampList)
+            setItem(JSON.stringify(newCampList))
         }
     }
 
     const updateCampaign = (newItem, index) => {
-        let updatedCampList = JSON.parse(localStorage.getItem('item'));
+        let updatedCampList = JSON.parse(item);
         if (index !== -1 && index !== null) {
             let newCampList = newItem;
             updatedCampList[index] = newCampList;
             localStorage.setItem('item', JSON.stringify(updatedCampList));
-            setItem(updatedCampList);
+            setItem(JSON.stringify(updatedCampList));
         }
     }
 
-    const CampList = CampaignList;
+    const deleteCampaign = (id) => {
+        const index = JSON.parse(item).findIndex(x => x.id === id);
+        if (index !== -1) {
+          const newArray = JSON.parse(item);
+          newArray.splice(index, 1);
+          setItem( JSON.stringify(newArray));
+          localStorage.setItem('item', JSON.stringify(newArray));
+        }
+    }
 
+    let CampList = item;
+    if(typeof CampList === 'string'){
+        CampList = JSON.parse(CampList)
+    }
     return (
         <div>
-            {CampList && CampList.map(item => <CampaignElement item={item}
+            {CampList && CampList.map(item => <CampaignElement key={item.id} item={item}
                 updateCampaign={(newItem, index) => updateCampaign(newItem, index)}
                 increasePoint={(index) => increasePoint(index)}
-                decreasePoint={(index) => decreasePoint(index)} />)}
+                decreasePoint={(index) => decreasePoint(index)}
+                deleteCampaign={(index) => deleteCampaign(index)} />)}
         </div>
     )
 
